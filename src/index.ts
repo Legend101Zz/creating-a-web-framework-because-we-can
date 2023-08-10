55228b5482320c8093edc029e32963efde240ffc
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-08-10 11:11:55 +0530

    added user model

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hi theree");
+import { User } from "./models/User";
+
+const user = new User({ name: "Mrigeshj", age: 10 });
+
+user.set({ name: "Mrigeshj", age: 100 });
+
+console.log(user.get("name"));
+console.log(user.get("age"));
